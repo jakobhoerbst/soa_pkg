39d6771460da51ef9675a162ce2e4103cb9f0818
jakobhoerbst <jakob@ubuntu>
2020-12-02 02:56:58 -0800

    random spawnen funktioniert

--- SOA.sh
+++ SOA.sh
@@ -14,9 +14,17 @@ killall -9 gzclient
 export TURTLEBOT3 MODELL=waffle_pi
 
 #sleep 5
-roslaunch soa_pkg project.launch&
+
+x_pos=$(bc <<< "scale=2;$(($RANDOM % 10))*1.25-5.625")
+y_pos=$(bc <<< "scale=2;$(($RANDOM % 10))*1.25-5.625")
+
+echo random x: $x_pos
+echo random y: $y_pos
+
+roslaunch soa_pkg project.launch x_pos:=$x_pos y_pos:=$y_pos&
 sleep 15
 roslaunch soa_pkg rvizLaunch.launch&
+
 #mate-terminal -e 'sh -c "roslaunch soa_pkg rvizLaunch.launch; exec bash"'
 
 #rviz -d rviz/rvizConfig.rviz &
@@ -26,5 +34,5 @@ roslaunch soa_pkg rvizLaunch.launch&
 #xterm -e 'rosrun hoerbst hoerbst __name:=JACUP /JACUP/odom:=/odom /JACUP/scan:=/scan' &
 #xterm -e 'roslaunch hoerbst remote.launch'
 
-mate-terminal -e 'sh -c "rosrun soa_pkg hoerbst; exec bash"'
-mate-terminal -e 'sh -c "rosrun soa_pkg navigation; exec bash"'
+#mate-terminal -e 'sh -c "rosrun soa_pkg hoerbst; exec bash"'
+#mate-terminal -e 'sh -c "rosrun soa_pkg navigation; exec bash"'
